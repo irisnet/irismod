a5f4b5d6c3219d6610846fae6904d5a9d491f03b
王迪 <wangdi@bianjie.ai>
2020-12-10 15:59:00 +0800

    add paginate to oracle

--- modules/oracle/keeper/grpc_query.go
+++ modules/oracle/keeper/grpc_query.go
@@ -2,6 +2,9 @@ package keeper
 
 import (
 	"context"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	gogotypes "github.com/gogo/protobuf/types"
 	"strings"
 
 	"google.golang.org/grpc/codes"
@@ -37,21 +40,46 @@ func (k Keeper) Feeds(c context.Context, req *types.QueryFeedsRequest) (*types.Q
 
 	state := strings.TrimSpace(req.State)
 	var result types.FeedsContext
+	var pageRes *query.PageResponse
+	var err error
+	store := ctx.KVStore(k.storeKey)
 	if len(state) == 0 {
-		k.IteratorFeeds(ctx, func(feed types.Feed) {
+		feedStore := prefix.NewStore(store, types.GetFeedPrefixKey())
+		pageRes, err = query.Paginate(feedStore, req.Pagination, func(key []byte, value []byte) error {
+			var feed types.Feed
+			err := k.cdc.UnmarshalBinaryBare(value, &feed)
+			if err != nil {
+				return err
+			}
 			result = append(result, BuildFeedContext(ctx, k, feed))
+			return nil
 		})
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "paginate: %v", err)
+		}
 	} else {
 		state, err := types.RequestContextStateFromString(req.State)
 		if err != nil {
 			return nil, status.Errorf(codes.InvalidArgument, "invalid request state")
 
 		}
-		k.IteratorFeedsByState(ctx, state, func(feed types.Feed) {
-			result = append(result, BuildFeedContext(ctx, k, feed))
+		feedStore := prefix.NewStore(store, types.GetFeedStatePrefixKey(state))
+		pageRes, err = query.Paginate(feedStore, req.Pagination, func(key []byte, value []byte) error {
+			var feedName gogotypes.StringValue
+			err := k.cdc.UnmarshalBinaryBare(value, &feedName)
+			if err != nil {
+				return err
+			}
+			if feed, found := k.GetFeed(ctx, feedName.Value); found {
+				result = append(result, BuildFeedContext(ctx, k, feed))
+			}
+			return nil
 		})
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "paginate: %v", err)
+		}
 	}
-	return &types.QueryFeedsResponse{Feeds: result}, nil
+	return &types.QueryFeedsResponse{Feeds: result, Pagination: pageRes}, nil
 }
 
 func (k Keeper) FeedValue(c context.Context, req *types.QueryFeedValueRequest) (*types.QueryFeedValueResponse, error) {
