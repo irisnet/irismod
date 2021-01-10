f3fa48629c72133606704c1b1da8c2561ba2ab63
张志强 <zhiqiang@bianjie.ai>
2021-01-10 20:47:25 +0800

    check standar demon

--- modules/coinswap/types/genesis.go
+++ modules/coinswap/types/genesis.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // NewGenesisState is the constructor function for GenesisState
 func NewGenesisState(params Params, denom string) *GenesisState {
 	return &GenesisState{
@@ -15,5 +21,11 @@ func DefaultGenesisState() *GenesisState {
 
 // ValidateGenesis - placeholder function
 func ValidateGenesis(data GenesisState) error {
+	if data.StandardDenom == "" {
+		return fmt.Errorf("coinswap standard denom can't be an empty string")
+	}
+	if err := sdk.ValidateDenom(data.StandardDenom); err != nil {
+		return err
+	}
 	return data.Params.Validate()
 }
