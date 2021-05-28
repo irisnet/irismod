8af86684ea9f677d0fafcb38fb157ed8294704c9
张志强 <zhiqiang@bianjie.ai>
2021-05-28 18:39:36 +0800

    remove rest api

--- modules/farm/client/rest/rest.go
+++ modules/farm/client/rest/rest.go
@@ -4,8 +4,6 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 )
 
 // Rest variable names
@@ -17,45 +15,4 @@ const (
 // RegisterHandlers defines routes that get registered by the main application
 func RegisterHandlers(cliCtx client.Context, r *mux.Router) {
 	registerTxRoutes(cliCtx, r)
-}
-
-type CreateFarmPoolReq struct {
-	BaseReq        rest.BaseReq `json:"base_req"` // base req
-	Name           string       `json:"name"`
-	Description    string       `json:"description,omitempty"`
-	LpTokenDenom   string       `json:"lp_token_denom"`
-	StartHeight    uint64       `json:"start_height"`
-	RewardPerBlock sdk.Coins    `json:"reward_per_block"`
-	TotalReward    sdk.Coins    `json:"total_reward"`
-	Destructible   bool         `json:"destructible,omitempty"`
-	Creator        string       `json:"creator"`
-}
-
-type DestroyFarmPoolReq struct {
-	BaseReq rest.BaseReq `json:"base_req"` // base req
-	Creator string       `json:"creator"`
-}
-
-type AppendRewardReq struct {
-	BaseReq  rest.BaseReq `json:"base_req"` // base req
-	PoolName string       `json:"pool_name"`
-	Reward   sdk.Coins    `json:"reward"`
-	Creator  string       `json:"creator"`
-}
-
-type StakeReq struct {
-	BaseReq rest.BaseReq `json:"base_req"` // base req
-	Amount  sdk.Coin     `json:"reward"`
-	Sender  string       `json:"creator"`
-}
-
-type UnstakeReq struct {
-	BaseReq rest.BaseReq `json:"base_req"` // base req
-	Amount  sdk.Coin     `json:"reward"`
-	Sender  string       `json:"creator"`
-}
-
-type HarvestReq struct {
-	BaseReq rest.BaseReq `json:"base_req"` // base req
-	Sender  string       `json:"creator"`
 }
