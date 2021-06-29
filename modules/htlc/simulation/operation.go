07542048e8f1192e5b06c00418e42fb86da5b0b2
mindcarver <626984947@qq.com>
2021-06-29 10:56:20 +0800

    add msgClaimHtlc

--- modules/htlc/simulation/operation.go
+++ modules/htlc/simulation/operation.go
@@ -1,6 +1,11 @@
 package simulation
 
 import (
+	"encoding/hex"
+	"math/rand"
+
+	tmbytes "github.com/tendermint/tendermint/libs/bytes"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/simapp/helpers"
@@ -8,10 +13,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
+
 	"github.com/irisnet/irismod/modules/htlc/keeper"
 	"github.com/irisnet/irismod/modules/htlc/types"
-	tmbytes "github.com/tendermint/tendermint/libs/bytes"
-	"math/rand"
 )
 
 // Simulation operation weights constants
@@ -74,9 +78,9 @@ func SimulateMsgCreateHtlc(ak types.AccountKeeper, bk types.BankKeeper) simtypes
 		if hasNeg {
 			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateHTLC, "Insufficient funds"), nil, nil
 		}
-		hashLock := "0000000000000000000000000000000000000000000000000000000000000000"
-		//hashLock := GenHashLock(r)
-		//tt := hex.EncodeToString(hashLock)
+		timestamp := uint64(0)
+		secret := Gensecret()
+		hashLock := hex.EncodeToString(GenHashLock(secret, timestamp))
 		timeLock := uint64(220)
 		tranfer := false
 		msg := &types.MsgCreateHTLC{
@@ -86,7 +90,7 @@ func SimulateMsgCreateHtlc(ak types.AccountKeeper, bk types.BankKeeper) simtypes
 			SenderOnOtherChain:   senderOnOtherChain.Address.String(),
 			Amount:               amount,
 			HashLock:             hashLock,
-			Timestamp:            uint64(0),
+			Timestamp:            timestamp,
 			TimeLock:             timeLock,
 			Transfer:             tranfer,
 		}
@@ -123,31 +127,33 @@ func SimulateMsgClaimHtlc(k keeper.Keeper, ak types.AccountKeeper, bk types.Bank
 		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
 
-		htlc := genRandomHtlc(ctx, k, r)
-		if htlc.Size() == 0{
-			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateHTLC, "not exist htlc"), nil, nil
+		htlc := GenRandomHtlc(ctx, k, r)
+		if htlc.Size() == 0 {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgClaimHTLC, "not exist htlc"), nil, nil
 		}
 		sender, err := sdk.AccAddressFromBech32(htlc.Sender)
-		if err != nil{
+		if err != nil {
 			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateHTLC, "invalid address"), nil, nil
 		}
-		account := ak.GetAccount(ctx, sender)
 
+		account := ak.GetAccount(ctx, sender)
 		simAccount, found := simtypes.FindAccount(accs, account.GetAddress())
 		if !found {
-			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateHTLC, "account not found"), nil, nil
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgClaimHTLC, "account not found"), nil, nil
 		}
 
-		secret, err := GenRandomSecret()
-		if err != nil{
-			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateHTLC, err.Error()), nil, err
+		if htlc.State != types.Open {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgClaimHTLC, "htlc not open"), nil, nil
 		}
-		msg := &types.MsgClaimHTLC{Sender: htlc.Sender, Id: htlc.Id, Secret: string(secret)}
+
+		secret := Gensecret()
+
+		msg := &types.MsgClaimHTLC{Sender: htlc.Sender, Id: htlc.Id, Secret: hex.EncodeToString(secret)}
 
 		spendable := bk.SpendableCoins(ctx, account.GetAddress())
 		fees, err := simtypes.RandomFees(r, ctx, spendable)
 		if err != nil {
-			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateHTLC, err.Error()), nil, err
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgClaimHTLC, err.Error()), nil, err
 		}
 
 		txGen := simappparams.MakeTestEncodingConfig().TxConfig
@@ -173,13 +179,11 @@ func SimulateMsgClaimHtlc(k keeper.Keeper, ak types.AccountKeeper, bk types.Bank
 	}
 }
 
-//func  GenHashLock(r *rand.Rand) []byte{
-//	hashlock ,_ := hex.DecodeString(simtypes.RandStringOfLength(r, 64))
-//	return hashlock
-//}
+func GenHashLock(secret tmbytes.HexBytes, timestamp uint64) []byte {
+	return types.GetHashLock(secret, timestamp)
+}
 
-
-func genRandomHtlc(ctx sdk.Context, k keeper.Keeper, r *rand.Rand)types.HTLC{
+func GenRandomHtlc(ctx sdk.Context, k keeper.Keeper, r *rand.Rand) types.HTLC {
 	htlcs := []types.HTLC{}
 	k.IterateHTLCs(
 		ctx,
@@ -188,19 +192,16 @@ func genRandomHtlc(ctx sdk.Context, k keeper.Keeper, r *rand.Rand)types.HTLC{
 			return false
 		},
 	)
-	if len(htlcs) == 0{
+	if len(htlcs) == 0 {
 		return types.HTLC{}
 	}
 	return htlcs[r.Intn(len(htlcs))]
 }
 
-
-func GenRandomSecret() ([]byte, error) {
-	bytes := make([]byte, 64)
-	if _, err := rand.Read(bytes); err != nil {
-		return []byte{}, err
+func Gensecret() []byte {
+	bytes := make([]byte, 32)
+	for i := 0; i < 32; i++ {
+		bytes[i] = 0
 	}
-	return bytes, nil
+	return bytes
 }
-
-
