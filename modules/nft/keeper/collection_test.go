d0ea147f4e843102240425ebd1d860a9bd349180
Dreamer <745124335@qq.com>
2021-11-29 10:48:45 +0800

    fix test

--- modules/nft/keeper/collection_test.go
+++ modules/nft/keeper/collection_test.go
@@ -1,76 +1,76 @@
 package keeper_test
 
-//import (
-//	"github.com/irisnet/irismod/modules/nft/keeper"
-//	"github.com/irisnet/irismod/modules/nft/types"
-//)
-//
-//func (suite *KeeperSuite) TestSetCollection() {
-//	nft := types.NewBaseNFT(tokenID, tokenNm, address, tokenURI, tokenData)
-//	// create a new NFT and add it to the collection created with the NFT mint
-//	nft2 := types.NewBaseNFT(tokenID2, tokenNm, address, tokenURI, tokenData)
-//
-//	denomE := types.Denom{
-//		Id:               denomID,
-//		Name:             denomNm,
-//		Schema:           schema,
-//		Creator:          address.String(),
-//		Symbol:           denomSymbol,
-//		MintRestricted:   true,
-//		UpdateRestricted: true,
-//	}
-//
-//	collection2 := types.Collection{
-//		Denom: denomE,
-//		NFTs:  []types.BaseNFT{nft2, nft},
-//	}
-//
-//	err := suite.keeper.SetCollection(suite.ctx, collection2)
-//	suite.Nil(err)
-//
-//	collection2, err = suite.keeper.GetCollection(suite.ctx, denomID)
-//	suite.NoError(err)
-//	suite.Len(collection2.NFTs, 2)
-//
-//	msg, fail := keeper.SupplyInvariant(suite.keeper)(suite.ctx)
-//	suite.False(fail, msg)
-//}
-//
-//func (suite *KeeperSuite) TestGetCollection() {
-//	// MintNFT shouldn't fail when collection does not exist
-//	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address)
-//	suite.NoError(err)
-//
-//	// collection should exist
-//	collection, err := suite.keeper.GetCollection(suite.ctx, denomID)
-//	suite.NoError(err)
-//	suite.NotEmpty(collection)
-//
-//	msg, fail := keeper.SupplyInvariant(suite.keeper)(suite.ctx)
-//	suite.False(fail, msg)
-//}
-//
-//func (suite *KeeperSuite) TestGetCollections() {
-//
-//	// MintNFT shouldn't fail when collection does not exist
-//	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address)
-//	suite.NoError(err)
-//
-//	msg, fail := keeper.SupplyInvariant(suite.keeper)(suite.ctx)
-//	suite.False(fail, msg)
-//}
-//
+import (
+	"github.com/irisnet/irismod/modules/nft/keeper"
+	"github.com/irisnet/irismod/modules/nft/types"
+)
+
+func (suite *KeeperSuite) TestSetCollection() {
+	nft := types.NewBaseNFT(tokenID, tokenNm, address, tokenURI, tokenData)
+	// create a new NFT and add it to the collection created with the NFT mint
+	nft2 := types.NewBaseNFT(tokenID2, tokenNm, address, tokenURI, tokenData)
+
+	denomE := types.Denom{
+		Id:               denomID,
+		Name:             denomNm,
+		Schema:           schema,
+		Creator:          address.String(),
+		Symbol:           denomSymbol,
+		MintRestricted:   true,
+		UpdateRestricted: true,
+	}
+
+	collection2 := types.Collection{
+		Denom: denomE,
+		NFTs:  []types.BaseNFT{nft2, nft},
+	}
+
+	err := suite.keeper.SetCollection(suite.ctx, collection2)
+	suite.Nil(err)
+
+	collection2, err = suite.keeper.GetCollection(suite.ctx, denomID)
+	suite.NoError(err)
+	suite.Len(collection2.NFTs, 2)
+
+	msg, fail := keeper.SupplyInvariant(suite.keeper)(suite.ctx)
+	suite.False(fail, msg)
+}
+
+func (suite *KeeperSuite) TestGetCollection() {
+	// MintNFT shouldn't fail when collection does not exist
+	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address, address)
+	suite.NoError(err)
+
+	// collection should exist
+	collection, err := suite.keeper.GetCollection(suite.ctx, denomID)
+	suite.NoError(err)
+	suite.NotEmpty(collection)
+
+	msg, fail := keeper.SupplyInvariant(suite.keeper)(suite.ctx)
+	suite.False(fail, msg)
+}
+
+func (suite *KeeperSuite) TestGetCollections() {
+
+	// MintNFT shouldn't fail when collection does not exist
+	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address, address)
+	suite.NoError(err)
+
+	msg, fail := keeper.SupplyInvariant(suite.keeper)(suite.ctx)
+	suite.False(fail, msg)
+}
+
 //func (suite *KeeperSuite) TestGetSupply() {
 //	// MintNFT shouldn't fail when collection does not exist
-//	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address)
+//	err := suite.keeper.MintNFT(suite.ctx, denomID, tokenID, tokenNm, tokenURI, tokenData, address, address)
 //	suite.NoError(err)
 //
 //	// MintNFT shouldn't fail when collection does not exist
-//	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID2, tokenNm2, tokenURI, tokenData, address2)
+//	err = suite.keeper.MintNFT(suite.ctx, denomID, tokenID2, tokenNm2, tokenURI, tokenData, address2, address)
 //	suite.NoError(err)
 //
 //	// MintNFT shouldn't fail when collection does not exist
-//	err = suite.keeper.MintNFT(suite.ctx, denomID2, tokenID, tokenNm2, tokenURI, tokenData, address2)
+//	err = suite.keeper.MintNFT(suite.ctx, denomID2, tokenID, tokenNm2, tokenURI, tokenData, address2, address2)
 //	suite.NoError(err)
 //
 //	supply := suite.keeper.GetTotalSupply(suite.ctx, denomID)
