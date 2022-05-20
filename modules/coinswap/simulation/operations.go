b3bafd078e7f7468f3f2f52ccda5e67c2a20344f
yuandu <taramaoriskykage@gmail.com>
2022-05-20 14:40:11 +0800

    add simulation and update docs

--- modules/coinswap/simulation/operations.go
+++ modules/coinswap/simulation/operations.go
@@ -2,7 +2,9 @@ package simulation
 
 import (
 	"fmt"
+	"math/big"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 
@@ -21,9 +23,11 @@ import (
 
 // Simulation operation weights constants
 const (
-	OpWeightMsgSwapOrder       = "op_weight_msg_swap_order"
-	OpWeightMsgAddLiquidity    = "op_weight_msg_add_liquidity"
-	OpWeightMsgRemoveLiquidity = "op_weight_msg_remove_liquidity"
+	OpWeightMsgSwapOrder                 = "op_weight_msg_swap_order"
+	OpWeightMsgAddLiquidity              = "op_weight_msg_add_liquidity"
+	OpWeightMsgRemoveLiquidity           = "op_weight_msg_remove_liquidity"
+	OpWeightMsgAddUnilateralLiquidity    = "op_weight_msg_add_unilateral_liquidity"
+	OpWeightMsgRemoveUnilateralLiquidity = "op_weight_msg_remove_unilateral_liquidity"
 )
 
 func WeightedOperations(
@@ -34,9 +38,11 @@ func WeightedOperations(
 	bk types.BankKeeper,
 ) simulation.WeightedOperations {
 	var (
-		weightSwap   int
-		weightAdd    int
-		weightRemove int
+		weightSwap             int
+		weightAdd              int
+		weightRemove           int
+		weightAddUnilateral    int
+		weightRemoveUnilateral int
 	)
 
 	appParams.GetOrGenerate(
@@ -60,6 +66,20 @@ func WeightedOperations(
 		},
 	)
 
+	appParams.GetOrGenerate(
+		cdc, OpWeightMsgAddUnilateralLiquidity, &weightAddUnilateral, nil,
+		func(_ *rand.Rand) {
+			weightAddUnilateral = 50
+		},
+	)
+
+	appParams.GetOrGenerate(
+		cdc, OpWeightMsgRemoveUnilateralLiquidity, &weightRemoveUnilateral, nil,
+		func(_ *rand.Rand) {
+			weightRemoveUnilateral = 50
+		},
+	)
+
 	return simulation.WeightedOperations{
 		simulation.NewWeightedOperation(
 			weightAdd,
@@ -73,6 +93,14 @@ func WeightedOperations(
 		simulation.NewWeightedOperation(
 			weightRemove,
 			SimulateMsgRemoveLiquidity(k, ak, bk),
+		),
+		simulation.NewWeightedOperation(
+			weightAddUnilateral,
+			SimulateMsgAddUnilateralLiquidity(k, ak, bk),
+		),
+		simulation.NewWeightedOperation(
+			weightRemoveUnilateral,
+			SimulateMsgRemoveUnilateralLiquidity(k, ak, bk),
 		),
 	}
 }
@@ -439,6 +467,231 @@ func SimulateMsgRemoveLiquidity(k keeper.Keeper, ak types.AccountKeeper, bk type
 
 		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
 
+	}
+}
+
+// SimulateMsgAddUnilateralLiquidity  simulates the addition of unilateral liquidity
+func SimulateMsgAddUnilateralLiquidity(k keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainId string,
+	) (
+		opMsg simtypes.OperationMsg, fOps []simtypes.FutureOperation, err error,
+	) {
+		// pick an account
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+		account := ak.GetAccount(ctx, simAccount.Address)
+
+		// pick a Coin from Coins
+		spendable := bk.SpendableCoins(ctx, account.GetAddress())
+		exactToken := RandomSpendableToken(r, spendable)
+		if !exactToken.Amount.IsPositive() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddUnilateralLiquidity, "exact token shall be positive"), nil, nil
+		}
+		if strings.HasPrefix(exactToken.Denom, types.LptTokenPrefix) {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddUnilateralLiquidity, "exact token shall not be lp token"), nil, nil
+		}
+
+		// pick a pool: pool-id is 1 if exactToken is IRIS, otherwise get the real pool-id
+		poolId := uint64(1)
+		if exactToken.Denom != k.GetStandardDenom(ctx) {
+			pool, exist := k.GetPool(ctx, exactToken.Denom)
+			if !exist {
+				return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "pool not found"), nil, err
+			}
+			poolId, _ = strconv.ParseUint(strings.TrimPrefix(pool.LptDenom, "lpt-"), 10, 64)
+		}
+
+		_, exist := k.GetPoolBySequenceId(ctx, poolId)
+		if !exist {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "pool not found"), nil, err
+		}
+
+		// minimum lpt amount
+		pool, _ := k.GetPoolBySequenceId(ctx, poolId)
+		balances, _ := k.GetPoolBalances(ctx, pool.EscrowAddress)
+
+		tokenBalanceAmt := balances.AmountOf(exactToken.Denom)
+		if !tokenBalanceAmt.IsPositive() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "exact token reserve amount is not enough"), nil, err
+		}
+		lptBalanceAmt := bk.GetSupply(ctx, pool.LptDenom).Amount
+		if !tokenBalanceAmt.IsPositive() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "lpt reserve amount is not enough"), nil, err
+		}
+		exactTokenAmt := exactToken.Amount
+
+		deltaFeeUnilateral := sdk.OneDec().Sub(k.GetParams(ctx).UnilateralLiquidityFee)
+		numerator := sdk.NewIntFromBigInt(deltaFeeUnilateral.BigInt())
+		denominator := sdk.NewIntWithDecimal(1, sdk.Precision)
+
+		square := denominator.Mul(tokenBalanceAmt).Add(numerator.Mul(exactTokenAmt)).Mul(lptBalanceAmt).Mul(lptBalanceAmt).Quo(denominator.Mul(tokenBalanceAmt))
+		if !square.IsPositive() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "fund is not enough"), nil, err
+		}
+		// lpt = square^0.5 - lpt_balance
+		var squareBigInt = &big.Int{}
+		squareBigInt.Sqrt(square.BigInt())
+		mintLptAmt := sdk.NewIntFromBigInt(squareBigInt).Sub(lptBalanceAmt)
+
+		deadline := randDeadline(r)
+
+		msg := types.NewMsgAddUnilateralLiquidity(
+			poolId,
+			exactToken,
+			mintLptAmt,
+			deadline,
+			account.GetAddress().String(),
+		)
+
+		// fee
+		var fees sdk.Coins
+		coinsTemp, isNeg := spendable.SafeSub(sdk.NewCoins(exactToken))
+		if !isNeg {
+			fees, err = simtypes.RandomFees(r, ctx, coinsTemp)
+			if err != nil {
+				return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate fees"), nil, nil
+			}
+		}
+
+		txGen := simappparams.MakeTestEncodingConfig().TxConfig
+		tx, err := helpers.GenTx(
+			txGen,
+			[]sdk.Msg{msg},
+			fees,
+			helpers.DefaultGenTxGas,
+			chainId,
+			[]uint64{account.GetAccountNumber()},
+			[]uint64{account.GetSequence()},
+			simAccount.PrivKey,
+		)
+
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate mock tx"), nil, err
+		}
+
+		if _, _, err := app.Deliver(txGen.TxEncoder(), tx); err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to deliver tx"), nil, err
+		}
+
+		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
+	}
+}
+
+// SimulateMsgRemoveUnilateralLiquidity  simulates the removal of unilateral liquidity
+func SimulateMsgRemoveUnilateralLiquidity(k keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainId string,
+	) (
+		opMsg simtypes.OperationMsg, fOps []simtypes.FutureOperation, err error,
+	) {
+		// pick an account
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+		account := ak.GetAccount(ctx, simAccount.Address)
+
+		// balances of account
+		spendable := bk.SpendableCoins(ctx, account.GetAddress())
+		if spendable.IsZero() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgSwapOrder, "spendable is zero"), nil, err
+		}
+
+		// pick a target token
+		targetToken := RandomSpendableToken(r, spendable)
+
+		// pick a pool: if target token is iris, select the pool-1
+		poolId := uint64(1)
+		if targetToken.Denom != k.GetStandardDenom(ctx) {
+			pool, exist := k.GetPool(ctx, targetToken.Denom)
+			if !exist {
+				return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "pool not found"), nil, err
+			}
+			poolId, _ = strconv.ParseUint(strings.TrimPrefix(pool.LptDenom, "lpt-"), 10, 64)
+		}
+
+		_, exist := k.GetPoolBySequenceId(ctx, poolId)
+		if !exist {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgAddLiquidity, "pool not found"), nil, err
+		}
+
+		pool, _ := k.GetPoolBySequenceId(ctx, poolId)
+		balances, err := k.GetPoolBalances(ctx, pool.EscrowAddress)
+
+		lptDenom := pool.LptDenom
+		targetTokenDenom := targetToken.Denom
+		counterpartTokenDenom := pool.CounterpartyDenom
+
+		if targetTokenDenom != pool.StandardDenom {
+			counterpartTokenDenom = pool.StandardDenom
+		}
+
+		// withdrawn liquidity
+		exactLpt := sdk.NewCoin(lptDenom, simtypes.RandomAmount(r, targetToken.Amount))
+		targetBalanceAmt := balances.AmountOf(targetTokenDenom)
+		counterpartBalanceAmt := balances.AmountOf(counterpartTokenDenom)
+		exactLptAmt := exactLpt.Amount
+		lptBalanceAmt := bk.GetSupply(ctx, lptDenom).Amount
+
+		if !exactLpt.IsValid() || !exactLptAmt.IsPositive() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgRemoveLiquidity, "invalid exact liquidity"), nil, nil
+		}
+		if lptBalanceAmt.LT(exactLptAmt) {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgRemoveLiquidity, "insufficient liquidity reserve"), nil, nil
+		}
+
+		targetWithdrawnAmt := targetBalanceAmt.Mul(exactLptAmt).Quo(lptBalanceAmt)
+		counterpartWithdrawnAmt := counterpartBalanceAmt.Mul(exactLptAmt).Quo(lptBalanceAmt)
+		targetSwapAmt := targetBalanceAmt.Sub(targetWithdrawnAmt).Mul(counterpartWithdrawnAmt).Quo(counterpartBalanceAmt)
+
+		deltaFeeUnilateral := sdk.OneDec().Sub(k.GetParams(ctx).UnilateralLiquidityFee)
+		numerator := sdk.NewIntFromBigInt(deltaFeeUnilateral.BigInt())
+		denominator := sdk.NewIntWithDecimal(1, sdk.Precision)
+		targetTokenAmt := targetWithdrawnAmt.Add(targetSwapAmt).Mul(numerator).Quo(denominator)
+
+		if targetBalanceAmt.LT(targetTokenAmt) {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgRemoveLiquidity, "insufficient target balance"), nil, nil
+		}
+
+		deadline := randDeadline(r)
+
+		msg := types.NewMsgRemoveUnilateralLiquidity(
+			poolId,
+			sdk.NewCoin(targetTokenDenom, targetTokenAmt),
+			exactLptAmt,
+			deadline,
+			account.GetAddress().String(),
+		)
+
+		var fees sdk.Coins
+		// ???
+		coinsTemp, hasNeg := spendable.SafeSub(sdk.NewCoins(sdk.NewCoin(lptDenom, exactLptAmt)))
+		if !hasNeg {
+			fees, err = simtypes.RandomFees(r, ctx, coinsTemp)
+			if err != nil {
+				return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate fees"), nil, nil
+			}
+		}
+
+		txGen := simappparams.MakeTestEncodingConfig().TxConfig
+
+		tx, err := helpers.GenTx(
+			txGen,
+			[]sdk.Msg{msg},
+			fees,
+			helpers.DefaultGenTxGas,
+			chainId,
+			[]uint64{account.GetAccountNumber()},
+			[]uint64{account.GetSequence()},
+			simAccount.PrivKey,
+		)
+
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to generate mock tx"), nil, err
+		}
+
+		if _, _, err := app.Deliver(txGen.TxEncoder(), tx); err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "unable to deliver tx"), nil, nil
+		}
+
+		return simtypes.NewOperationMsg(msg, true, "", nil), nil, nil
 	}
 }
 
