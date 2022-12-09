fbb590c57aefc5776b8e1c3d9ba403068da1b01e
Dreamer <745124335@qq.com>
2022-12-09 15:41:47 +0800

    refactor code

--- modules/nft/keeper/ics721.go
+++ modules/nft/keeper/ics721.go
@@ -4,27 +4,13 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/x/nft"
 	nftkeeper "github.com/cosmos/cosmos-sdk/x/nft/keeper"
 
 	"github.com/irisnet/irismod/modules/nft/types"
 )
-
-var _ NFTKeeper = (*ICS721Keeper)(nil)
-
-// NFTKeeper is a inteface for ics721 protocol
-type NFTKeeper interface {
-	SaveClass(ctx sdk.Context, class nft.Class) error
-	Mint(ctx sdk.Context, token nft.NFT, receiver sdk.AccAddress) error
-	Transfer(ctx sdk.Context, classID string, nftID string, receiver sdk.AccAddress) error
-	Burn(ctx sdk.Context, classID string, nftID string) error
-
-	GetOwner(ctx sdk.Context, classID string, nftID string) sdk.AccAddress
-	HasClass(ctx sdk.Context, classID string) bool
-	GetClass(ctx sdk.Context, classID string) (nft.Class, bool)
-	GetNFT(ctx sdk.Context, classID, nftID string) (nft.NFT, bool)
-}
 
 // AccountKeeper defines the contract required for account APIs.
 type AccountKeeper interface {
@@ -36,17 +22,18 @@ type AccountKeeper interface {
 
 // ICS721Keeper defines the ICS721 Keeper
 type ICS721Keeper struct {
+	nftkeeper.Keeper
+	k   Keeper
 	cdc codec.Codec
-	nk  nftkeeper.Keeper
 	ak  AccountKeeper
 }
 
 // NewISC721Keeper creates a new ics721 Keeper instance
-func (k Keeper) NewISC721Keeper(ak AccountKeeper) ICS721Keeper {
+func NewISC721Keeper(k Keeper, ak AccountKeeper) ICS721Keeper {
 	return ICS721Keeper{
-		cdc: k.cdc,
-		nk:  k.nk,
-		ak:  ak,
+		Keeper: k.nk,
+		cdc:    k.cdc,
+		ak:     ak,
 	}
 }
 
@@ -62,10 +49,8 @@ func (ik ICS721Keeper) SaveClass(ctx sdk.Context, class nft.Class) error {
 	//TODO Because ics721 protocol is not currently supported the transfer of classData ,
 	// the original classData will be ignored now
 	var denomMetadata = &types.DenomMetadata{
-		Creator:        moduleAddress.String(),
-		MintRestricted: false,
-		// NOTICE: UpdateRestricted is set to false to prevent nft from being edited on the destination chain,
-		// but when transferring to the original chain, the edited information is lost
+		Creator:          moduleAddress.String(),
+		MintRestricted:   false,
 		UpdateRestricted: false,
 	}
 	metadata, err := codectypes.NewAnyWithValue(denomMetadata)
@@ -73,45 +58,19 @@ func (ik ICS721Keeper) SaveClass(ctx sdk.Context, class nft.Class) error {
 		return err
 	}
 	class.Data = metadata
-	return ik.nk.SaveClass(ctx, class)
+	return ik.Keeper.SaveClass(ctx, class)
 }
 
-// Mint implement the method of ICS721Keeper.Mint
-func (ik ICS721Keeper) Mint(ctx sdk.Context, token nft.NFT, receiver sdk.AccAddress) error {
-	metadata, err := codectypes.NewAnyWithValue(&types.NFTMetadata{})
+// Transfer implement the method of ICS721Keeper.Update
+func (ik ICS721Keeper) Update(ctx sdk.Context, token nft.NFT) error {
+	denomInfo, err := ik.k.GetDenomInfo(ctx, token.ClassId)
 	if err != nil {
 		return err
 	}
-	token.Data = metadata
-	return ik.nk.Mint(ctx, token, receiver)
-}
 
-// Transfer implement the method of ICS721Keeper.Transfer
-func (ik ICS721Keeper) Transfer(ctx sdk.Context, classID string, nftID string, receiver sdk.AccAddress) error {
-	return ik.nk.Transfer(ctx, classID, nftID, receiver)
-}
-
-// Burn implement the method of ICS721Keeper.Burn
-func (ik ICS721Keeper) Burn(ctx sdk.Context, classID string, nftID string) error {
-	return ik.nk.Burn(ctx, classID, nftID)
-}
-
-// GetOwner implement the method of ICS721Keeper.GetOwner
-func (ik ICS721Keeper) GetOwner(ctx sdk.Context, classID string, nftID string) sdk.AccAddress {
-	return ik.nk.GetOwner(ctx, classID, nftID)
-}
-
-// HasClass implement the method of ICS721Keeper.HasClass
-func (ik ICS721Keeper) HasClass(ctx sdk.Context, classID string) bool {
-	return ik.nk.HasClass(ctx, classID)
-}
-
-// GetClass implement the method of ICS721Keeper.GetClass
-func (ik ICS721Keeper) GetClass(ctx sdk.Context, classID string) (nft.Class, bool) {
-	return ik.nk.GetClass(ctx, classID)
-}
-
-// GetNFT implement the method of ICS721Keeper.GetNFT
-func (ik ICS721Keeper) GetNFT(ctx sdk.Context, classID, nftID string) (nft.NFT, bool) {
-	return ik.nk.GetNFT(ctx, classID, nftID)
+	if denomInfo.UpdateRestricted {
+		// if true , nobody can update the NFT under this denom
+		return sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "nobody can update the NFT under this denom %s", token.ClassId)
+	}
+	return ik.Update(ctx, token)
 }
