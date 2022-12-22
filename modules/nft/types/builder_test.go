e0b06461dbc34e5064dcb3098d094189f2f94bd2
Dreamer <745124335@qq.com>
2022-12-22 14:26:09 +0800

    refactor nft builder uint test

--- modules/nft/types/builder_test.go
+++ modules/nft/types/builder_test.go
@@ -1,79 +1,60 @@
 package types
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"testing"
-
-	"github.com/tendermint/tendermint/crypto"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/nft"
-	"github.com/golang/protobuf/proto"
+	"github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto"
 )
 
-func TestClassMetadataResolverEncodeAndDecode(t *testing.T) {
-	creator, err := sdk.AccAddressFromHexUnsafe(crypto.AddressHash([]byte("test_consumer")).String())
-	require.NoError(t, err, "AccAddressFromHexUnsafe failed")
-
-	// dataMap := map[string]MediaField{
-	// 	"irismod:key1": {Value: "value1"},
-	// 	"irismod:key2": {Value: "value2"},
-	// }
-	// dataBytes, err := json.Marshal(dataMap)
-	// require.NoError(t, err, " dataMap json.Marshal failed")
-	// t.Logf("%s", dataBytes)
-
-	denomMetadata := DenomMetadata{
-		Creator:          creator.String(),
-		Schema:           "{}",
-		MintRestricted:   true,
-		UpdateRestricted: true,
-		Data:             "{\"key1\":\"value1\",\"key2\":\"value2\"}",
+func TestTokenBuilder_Build(t *testing.T) {
+	nftMetadata := NFTMetadata{
+		Name: "My Cat",
+		Data: "{\"key1\":\"value1\",\"key2\":\"value2\"}",
 	}
 
-	bz, err := json.Marshal(denomMetadata)
-	require.NoError(t, err, " denomMetadata json.Marshal failed")
+	bz, err := json.Marshal(nftMetadata)
+	require.NoError(t, err, " nftMetadata json.Marshal failed")
 	t.Logf("%s", bz)
 
-	any, err := codectypes.NewAnyWithValue(&denomMetadata)
-	require.NoError(t, err, " denomMetadata codectypes.NewAnyWithValue failed")
+	any, err := codectypes.NewAnyWithValue(&nftMetadata)
+	require.NoError(t, err, " nftMetadata codectypes.NewAnyWithValue failed")
 
-	getModuleAddress := func(_ string) sdk.AccAddress {
-		return creator
-	}
-	class := nft.Class{
-		Id:          "cat",
-		Name:        "kitty",
-		Symbol:      "symbol",
-		Description: "digital cat",
-		Uri:         "uri",
-		UriHash:     "uri_hash",
-		Data:        any,
+	token := nft.NFT{
+		ClassId: "kitty",
+		Id:      "cat",
+		Uri:     "uri",
+		UriHash: "uri_hash",
+		Data:    any,
 	}
 
 	cdc := GetEncoding()
-	bz, err = cdc.MarshalJSON(&class)
-	require.NoError(t, err, " class MarshalJSON failed")
-	t.Logf("class:%s", bz)
+	bz, err = cdc.MarshalJSON(&token)
+	require.NoError(t, err, " token MarshalJSON failed")
+	t.Logf("%s", bz)
 
-	builder := NewClassBuilder(cdc, getModuleAddress)
-	result, err := builder.BuildMetadata(class)
-	require.NoError(t, err, " class resolver.GetMetadata failed")
+	builder := NewTokenBuilder(cdc)
+	result, err := builder.BuildMetadata(token)
+	require.NoError(t, err, " token builder.BuildMetadata failed")
 	t.Log(result)
 
-	expClass, err := builder.Build(class.Id, class.Uri, result)
-	require.NoError(t, err, " class resolver.Decode failed")
+	expToken, err := builder.Build(token.ClassId, token.Id, token.Uri, result)
+	require.NoError(t, err, " token builder.Build failed")
 
-	exp, err := cdc.MarshalInterfaceJSON(&class)
-	require.NoError(t, err, " class resolver.Decode failed")
+	exp, err := cdc.MarshalInterfaceJSON(&token)
+	require.NoError(t, err, " token cdc.MarshalInterfaceJSON failed")
 	t.Logf("%s", exp)
 
-	act, err := cdc.MarshalInterfaceJSON(&expClass)
-	require.NoError(t, err, " class resolver.Decode failed")
+	act, err := cdc.MarshalInterfaceJSON(&expToken)
+	require.NoError(t, err, " token cdc.MarshalInterfaceJSON failed")
 	t.Logf("%s", act)
 
 	require.Equal(t, act, exp, "not equal")
@@ -90,4 +71,177 @@ func GetEncoding() codec.Codec {
 	)
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	return marshaler
+}
+
+func TestClassBuilder_BuildMetadata(t *testing.T) {
+	creator, err := sdk.AccAddressFromHexUnsafe(crypto.AddressHash([]byte("test_consumer")).String())
+	require.NoError(t, err, "AccAddressFromHexUnsafe failed")
+
+	cdc := GetEncoding()
+	getModuleAddress := func(_ string) sdk.AccAddress {
+		return creator
+	}
+	class := nft.Class{
+		Name:        "kitty",
+		Symbol:      "symbol",
+		Description: "digital cat",
+		Uri:         "uri",
+		UriHash:     "uri_hash",
+	}
+	denomMetadata := DenomMetadata{
+		Creator:          creator.String(),
+		Schema:           "{}",
+		MintRestricted:   true,
+		UpdateRestricted: true,
+	}
+
+	type args struct {
+		classID   string
+		classData string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "empty classData",
+			args: args{
+				classID:   "cat",
+				classData: "",
+			},
+			want:    `{"irismod:creator":{"value":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y"},"irismod:description":{"value":"digital cat"},"irismod:mint_restricted":{"value":true},"irismod:name":{"value":"kitty"},"irismod:schema":{"value":"{}"},"irismod:symbol":{"value":"symbol"},"irismod:update_restricted":{"value":true},"irismod:uri_hash":{"value":"uri_hash"}}`,
+			wantErr: false,
+		},
+		{
+			name: "classData is invalid json string",
+			args: args{
+				classID:   "cat",
+				classData: "hhaahha",
+			},
+			want:    `{"irismod:creator":{"value":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y"},"irismod:description":{"value":"digital cat"},"irismod:mint_restricted":{"value":true},"irismod:name":{"value":"kitty"},"irismod:schema":{"value":"{}"},"irismod:symbol":{"value":"symbol"},"irismod:update_restricted":{"value":true},"irismod:uri_hash":{"value":"uri_hash"}}`,
+			wantErr: false,
+		},
+		{
+			name: "classData is valid json string",
+			args: args{
+				classID:   "cat",
+				classData: "{\"key1\":\"value1\",\"key2\":\"value2\"}",
+			},
+			want:    `{"irismod:creator":{"value":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y"},"irismod:description":{"value":"digital cat"},"irismod:mint_restricted":{"value":true},"irismod:name":{"value":"kitty"},"irismod:schema":{"value":"{}"},"irismod:symbol":{"value":"symbol"},"irismod:update_restricted":{"value":true},"irismod:uri_hash":{"value":"uri_hash"},"key1":"value1","key2":"value2"}`,
+			wantErr: false,
+		},
+		{
+			name: "class is IBC assets and classData is invalid json string",
+			args: args{
+				classID:   "ibc/943B966B2B8A53C50A198EDAB7C9A41FCEAF24400A94167846679769D8BF8311",
+				classData: "hahhahha",
+			},
+			want:    `hahhahha`,
+			wantErr: false,
+		},
+		{
+			name: "class is IBC assets and classData is valid json string",
+			args: args{
+				classID:   "ibc/943B966B2B8A53C50A198EDAB7C9A41FCEAF24400A94167846679769D8BF8311",
+				classData: "{\"key1\":\"value1\",\"key2\":\"value2\"}",
+			},
+			want:    `{"irismod:creator":{"value":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y"},"irismod:description":{"value":"digital cat"},"irismod:mint_restricted":{"value":true},"irismod:name":{"value":"kitty"},"irismod:schema":{"value":"{}"},"irismod:symbol":{"value":"symbol"},"irismod:update_restricted":{"value":true},"irismod:uri_hash":{"value":"uri_hash"},"key1":"value1","key2":"value2"}`,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cb := ClassBuilder{
+				cdc:              cdc,
+				getModuleAddress: getModuleAddress,
+			}
+
+			denomMetadata.Data = tt.args.classData
+			any, err := codectypes.NewAnyWithValue(&denomMetadata)
+			require.NoError(t, err, " denomMetadata codectypes.NewAnyWithValue failed")
+
+			class.Id = tt.args.classID
+			class.Data = any
+
+			got, err := cb.BuildMetadata(class)
+			want := base64.RawStdEncoding.EncodeToString([]byte(tt.want))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ClassBuilder.BuildMetadata() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != want {
+				t.Errorf("ClassBuilder.BuildMetadata() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClassBuilder_Build(t *testing.T) {
+	creator, err := sdk.AccAddressFromHexUnsafe(crypto.AddressHash([]byte("test_consumer")).String())
+	require.NoError(t, err, "AccAddressFromHexUnsafe failed")
+
+	cdc := GetEncoding()
+	getModuleAddress := func(_ string) sdk.AccAddress {
+		return creator
+	}
+	classID := "cat"
+	classURI := "uri"
+
+	type args struct {
+		classData string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "empty classData",
+			args: args{
+				classData: `{"irismod:creator":{"value":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y"},"irismod:description":{"value":"digital cat"},"irismod:mint_restricted":{"value":true},"irismod:name":{"value":"kitty"},"irismod:schema":{"value":"{}"},"irismod:symbol":{"value":"symbol"},"irismod:update_restricted":{"value":true},"irismod:uri_hash":{"value":"uri_hash"}}`,
+			},
+			want:    `{"@type":"/cosmos.nft.v1beta1.Class","id":"cat","name":"kitty","symbol":"symbol","description":"digital cat","uri":"uri","uri_hash":"uri_hash","data":{"@type":"/irismod.nft.DenomMetadata","creator":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y","schema":"{}","mint_restricted":true,"update_restricted":true,"data":"{}"}}`,
+			wantErr: false,
+		},
+		{
+			name: "classData is invalid json string",
+			args: args{
+				classData: `this is empty class data`,
+			},
+			want:    `{"@type":"/cosmos.nft.v1beta1.Class","id":"cat","name":"","symbol":"","description":"","uri":"uri","uri_hash":"","data":{"@type":"/irismod.nft.DenomMetadata","creator":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y","schema":"","mint_restricted":true,"update_restricted":true,"data":"this is empty class data"}}`,
+			wantErr: false,
+		},
+		{
+			name: "classData is valid json string",
+			args: args{
+				classData: `{"irismod:creator":{"value":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y"},"irismod:description":{"value":"digital cat"},"irismod:mint_restricted":{"value":true},"irismod:name":{"value":"kitty"},"irismod:schema":{"value":"{}"},"irismod:symbol":{"value":"symbol"},"irismod:update_restricted":{"value":true},"irismod:uri_hash":{"value":"uri_hash"},"key1":"value1","key2":"value2"}`,
+			},
+			want:    `{"@type":"/cosmos.nft.v1beta1.Class","id":"cat","name":"kitty","symbol":"symbol","description":"digital cat","uri":"uri","uri_hash":"uri_hash","data":{"@type":"/irismod.nft.DenomMetadata","creator":"cosmos1lz57ae4uuk7qg0jlampt4me4t7rahlxl5pnn3y","schema":"{}","mint_restricted":true,"update_restricted":true,"data":"{\"key1\":\"value1\",\"key2\":\"value2\"}"}}`,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cb := ClassBuilder{
+				cdc:              cdc,
+				getModuleAddress: getModuleAddress,
+			}
+			classDataRaw := base64.RawStdEncoding.EncodeToString([]byte(tt.args.classData))
+			result, err := cb.Build(classID, classURI, classDataRaw)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ClassBuilder.BuildMetadata() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			got, err := cdc.MarshalInterfaceJSON(&result)
+			require.NoError(t, err, " class cdc.MarshalInterfaceJSON failed")
+
+			if string(got) != tt.want {
+				t.Errorf("ClassBuilder.BuildMetadata() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
